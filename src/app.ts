0f09c24af3e87f8030702e238ba0e9942c4422c6
Shkuro Yuri <yury.shkuro@x5.ru>
2024-02-02 12:20:52 +0300

    chore: Добавил модель юзера и подключил БД

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mydb');
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
